bc2122a838f10b0c7e71b2ad73452b58b840f5e9
tvh050423@gmail.com <tvh050423@gmail.com>
2024-09-18 17:29:23 +0700

    check: dbconfig

--- app/config/db.js
+++ app/config/db.js
@@ -4,6 +4,7 @@ const dbConfig = require("../config/db.config.js");
 var connection = mysql.createConnection(dbConfig);
 
 connection.connect((error) => {
+  console.log(dbConfig);
   if (error) {
     console.log(
       "Error connecting to the database: " + error.code + error.message
